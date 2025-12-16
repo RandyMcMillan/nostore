c274e583a846c21a6112733fa0ba668168970743
randymcmillan <randymcmillan@protonmail.com>
2025-12-16 14:41:44 -0500

    build_safari_extension.sh

--- build_safari_extension.sh
+++ build_safari_extension.sh
@@ -8,10 +8,21 @@ DESTINATION="platform=macOS"
 
 echo "Building Xcode project: $PROJECT, Scheme: $SCHEME, Configuration: $CONFIGURATION"
 
+npm run watch-tailwind &
+sleep 5
+
 xcodebuild -project "$PROJECT" \
            -scheme "$SCHEME" \
            -configuration "$CONFIGURATION" \
            -destination "$DESTINATION" \
+           clean
+
+xcodebuild -project "$PROJECT" \
+           -scheme "$SCHEME" \
+           -configuration "$CONFIGURATION" \
+           -destination "$DESTINATION" \
+           ARCHS="arm64 x86_64" \
+           ONLY_ACTIVE_ARCH=NO \
            build
 
 if [ $? -eq 0 ]; then
